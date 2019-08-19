f6ce99a7b358e8b2371534fec928acebfe54095b
Rafael D.Carmo <rafaeldocarmo2010@gmail.com>
2019-08-19 16:51:12 -0300

    configurações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCxb4lfrpY3NnhZzzUakDMREUt5rBlGuyo",
+  authDomain: "portifolio-rafa.firebaseapp.com",
+  databaseURL: "https://portifolio-rafa.firebaseio.com",
+  projectId: "portifolio-rafa",
+  storageBucket: "portifolio-rafa.appspot.com",
+  messagingSenderId: "54950511803",
+  appId: "1:54950511803:web:f220d0f29b8db067"}
 };
